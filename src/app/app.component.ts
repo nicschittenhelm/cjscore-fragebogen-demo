ecaaa59a6f81071b8e151132889f8624ed761d03
Schittenhelm, Nic <nic.schittenhelm@exxeta.com>
2024-03-27 14:57:53 +0100

    working behavior to display prev or next section based on scroll

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, ElementRef, QueryList, ViewChild, ViewChildren } from '@angular/core';
-import { Subject, debounceTime, distinctUntilChanged, filter, fromEvent, map, retryWhen, takeUntil } from 'rxjs';
+import { Subject, debounceTime, distinctUntilChanged, filter, fromEvent, map, retryWhen, takeUntil, throttleTime } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -9,43 +9,54 @@ import { Subject, debounceTime, distinctUntilChanged, filter, fromEvent, map, re
 export class AppComponent {
   title = 'cjscore-fragebogen-demo';
 
-  //distinctUntilChanged(), // Only emit when direction changes
-
-
   @ViewChild('section', { static: false }) section?: ElementRef;
+  @ViewChildren('section') sectionElements?: QueryList<ElementRef>;
   private destroy$ = new Subject<void>();
   private scrollReachedEdge:boolean = false;
-  
+  currentSectionIndex = 0;
+  sectionsArr: any[] = ['magna', 'ipsum', 'dolor', 'sit', 'amet'];
+  //*ngIf="i === currentSectionIndex"
 
   ngOnDestroy(): void {
     this.destroy$.next();
     this.destroy$.complete();
   }
 
-  ngAfterViewInit(): void {
-    fromEvent<WheelEvent>(this.section?.nativeElement, 'wheel')
-      .pipe(
-        map((event) => this.getWheelDirection(event)),
-        filter((direction) => {
-          if (!this.sectionIsOverflowing()) {
-            return true;
-          } else if (this.sectionIsOverflowing()) {
-            const scrollStatus = this.scrollStatus(this.section?.nativeElement);
-            return (scrollStatus === 'up' && direction === 'up') || (scrollStatus === 'down' && direction === 'down');
-          }
-          return false;
-        }),
-        distinctUntilChanged(),
-        takeUntil(this.destroy$)
-      )
-      .subscribe((direction) => {
-        this.triggerAction(direction);
-      });
+  ngAfterViewChecked(): void {
+    
+    this.sectionElements?.forEach((element, index) => {
+      //const sectionElement = this.sectionElements?.toArray()[index]?.nativeElement;
+      const sectionElement = element.nativeElement
+
+      if (!sectionElement.wheelListenerAdded) {
+        fromEvent<WheelEvent>(sectionElement, 'wheel')
+          .pipe(
+            map((event) => this.getWheelDirection(event)),
+            filter((direction) => {
+              if (!this.sectionIsOverflowing(sectionElement)) {
+                return true;
+              } else if (this.sectionIsOverflowing(sectionElement)) {
+                const scrollStatus = this.scrollStatus(sectionElement);
+                return (scrollStatus === 'up' && direction === 'up') || (scrollStatus === 'down' && direction === 'down');
+              }
+              return false;
+            }),
+            throttleTime(200), // Add a 200ms timeout
+            debounceTime(0), // Add a short delay (adjust the time as needed)
+            distinctUntilChanged(),
+            takeUntil(this.destroy$)
+            )
+            .subscribe((direction) => {
+              this.triggerAction(direction);
+            });
+        // Mark the section as having the wheel listener added
+        sectionElement.wheelListenerAdded = true;
+      }
+    });
   }
 
-  sectionIsOverflowing(): boolean {
-    const sectionElement = this.section?.nativeElement;
-    return sectionElement && sectionElement.scrollHeight > sectionElement.clientHeight;
+  sectionIsOverflowing(sectionElement: HTMLElement): boolean {
+    return sectionElement.scrollHeight > sectionElement.clientHeight;
   }
 
   private scrollStatus(sectionElement: HTMLElement) {
@@ -73,6 +84,15 @@ export class AppComponent {
 
   triggerAction(direction: 'up' | 'down'): void {
     console.log('Scroll direction:', direction);
+    console.log('Current section index:', this.currentSectionIndex);
+    
+    if (direction === 'up' && this.currentSectionIndex > 0) {
+      this.currentSectionIndex--; // Move to the previous section if available
+    } else if (direction === 'down' && this.currentSectionIndex < this.sectionsArr.length - 1) {
+      this.currentSectionIndex++; // Move to the next section if available
+    }
+    console.log('New section index:', this.currentSectionIndex);
+    
   }
 
 
